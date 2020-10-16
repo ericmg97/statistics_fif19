a2a459be859a4e6dbd2c0878247c557480909259
Eric <e.martin@estudiantes.matcom.uh.cu>
2020-10-16 14:59:49 -0400

    Add Cluster and CART

---
+++ c_tree.R
@@ -0,0 +1,52 @@
+library(rpart)
+library(rpart.plot)
+library(digest)
+l<-length(players_19$sofifa_id)
+
+#Selección de un subconjunto de variables, manejo de valores faltantes,
+#conversión de las clases objetivos a enteros y normalización de datos
+subconjunto_con_id = subset(players_19, select = -c(player_url, short_name, long_name, dob, real_face, joined, contract_valid_until, nation_position, nation_jersey_number, team_jersey_number, player_traits, player_tags, player_positions, ls, st, rs, lw, lf,cf,rf,rw,lam,cam,ram, lm, lcm, cm,rcm,rm,lwb,ldm,cdm,rdm,rwb,lb,lcb,cb,rcb,rb))
+subconjuntonumerico=subset(subconjunto_con_id, select=-c(nationality,club,preferred_foot, work_rate, body_type, team_position, loaned_from))
+subconjuntonominal=subset(subconjunto_con_id, select=c(sofifa_id,nationality,club,preferred_foot, work_rate, body_type, team_position, loaned_from))
+subconjuntoresultado=subset(subconjunto_con_id, select=c(sofifa_id,team_position))
+
+subconjuntonumerico[is.na(subconjuntonumerico)]<-0
+subconjuntonumerico<-sapply(subconjuntonumerico,as.numeric)
+subconjuntonumerico[is.na(subconjuntonumerico)]<-0
+
+subconjunto<-merge(subconjuntonumerico,subconjuntoresultado)
+targetob<-digest2int(subconjunto$team_position)#Conversión de las clases objetivos de string a enteros
+subconjunto$target<-targetob
+subconjunto=subset(subconjunto, select = -c(sofifa_id,team_position))
+subconjunto[is.na(subconjunto)]<-"No"
+
+#Elección del subconjunto de entrenamiento
+sub<-sample(1:l,10000)
+sub
+
+subconjunto[sub,]#Muestra de entrenamiento
+
+subconjunto[-sub,]#Muestra de prueba
+
+#Generación del CART
+subconjunto.tree <- rpart(formula=target~. , data=subconjunto[sub,], cp=0.01, maxdepth=20, method = "class")
+
+summary(subconjunto.tree)
+subconjunto.tree
+
+#Graficando el árbol resultante
+rpart.plot(subconjunto.tree)
+
+
+#Verificando en la muestra de prueba el CART
+subconjunto.pred <- predict(subconjunto.tree, newdata = subconjunto[-sub,],type="class")
+subconjunto.pred
+positions<-subconjunto[-sub,]$target
+
+#Tabla de confusión
+tb <- table(subconjunto.pred,subconjunto[-sub,]$target)
+
+error.rpart<- 1-(sum(diag(tb))/sum(tb))#Cálculo del error
+tb
+error.rpart
+
---
+++ cluster.R
@@ -0,0 +1,35 @@
+l<-length(players_19$sofifa_id)
+sub<-sample(1:l,2000)#Eleccion de una muestra de la poblaciÃ³n
+
+#SelecciÃ³n de un subconjunto de variables, manejo de valores faltantes
+#y normalizaciÃ³n de datos
+subconjunto_con_id = subset(players_19, select = -c(player_url, short_name, long_name, dob, real_face, joined, contract_valid_until, nation_position, nation_jersey_number, team_jersey_number, player_traits, player_tags, player_positions, ls, st, rs, lw, lf,cf,rf,rw,lam,cam,ram, lm, lcm, cm,rcm,rm,lwb,ldm,cdm,rdm,rwb,lb,lcb,cb,rcb,rb))
+subconjunto=subset(subconjunto_con_id, select=-c(sofifa_id,nationality,club,preferred_foot, work_rate, body_type, team_position, loaned_from))
+subconjunto2<-subconjunto
+subconjunto2[is.na(subconjunto2)]<-0
+subconjunto2<-sapply(subconjunto2,as.numeric)
+subconjunto2[is.na(subconjunto2)]<-0
+scaled.data <- scale(subconjunto2[sub,])
+head(scaled.data)
+
+scaled.data[sub,]#Muestra
+
+d<-dist(scaled.data, method = "euclidean")#CÃ¡lculo de la matriz de distancia
+fit <- hclust(d,method = "complete")#AplicaciÃ³n de la tÃ©cnica de clÃºster jerÃ¡rquico
+d2<- as.dendrogram(fit)
+d2
+
+#GraficaciÃ³n del clÃºster jerÃ¡rquico
+plot(d2)
+rect.hclust(fit,k=10,border = "red")
+
+fit.kmeans<-kmeans(scaled.data, 10)#AplicaciÃ³n de la tÃ©cnica de clÃºster por k-means
+fit.kmeans
+
+#GraficaciÃ³n de los centros de los clusters atendiendo a diferentes variables
+plot((fit.kmeans$centers[1:10,1]),(fit.kmeans$centers[1:10,4]), col= 1:5, pch=1:4, xlab = "age", ylab = "overall")
+plot((fit.kmeans$centers[1:10,1]),(fit.kmeans$centers[1:10,7]), col= 1:5, pch=1:4, xlab = "age", ylab = "wage")
+plot((fit.kmeans$centers[1:10,1]),(fit.kmeans$centers[1:10,6]), col= 1:5, pch=1:4, xlab = "age", ylab = "value")
+plot((fit.kmeans$centers[1:10,4]),(fit.kmeans$centers[1:10,8]), col= 1:5, pch=1:4, xlab = "overall", ylab = "reputation")
+plot((fit.kmeans$centers[1:10,2]),(fit.kmeans$centers[1:10,34]), col= 1:5, pch=1:4, xlab = "height", ylab = "speed")
+plot((fit.kmeans$centers[1:10,7]),(fit.kmeans$centers[1:10,6]), col= 1:5, pch=1:4, xlab = "wage", ylab = "value")
